494e643e61fab3c7a446668edaafdb331d67e717
NathanMoore <NathanMoore@4d56c4e7-3306-40bb-bc25-9f1c267d86e0>
2013-12-05 18:49:46 +0000

    Created external tileset for maps.  This should make it easier to create multiple maps using the same tileset as well as allowing changes to the tileset be applied to all maps that use it.
    To use an external tileset in Tiled, use Map -> Add External Tileset.
    
    To modify an existing map to use an external tileset, change the tileset tag to <tileset firstgid="1" source="tileset.tsx"/> and remove the stuff inside the tags (along with the closing tag).
    
    To modify the external tileset, open tileset.tmx, make changes, then click the Export Tileset As button in the tileset frame.  Then reload the map you were working on.
    
    git-svn-id: file:///var/vcs/repos/svn/trunk@41 4d56c4e7-3306-40bb-bc25-9f1c267d86e0

---
+++ game/data/maps/tileset.tsx
@@ -0,0 +1,354 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="32" tileheight="32">
+ <image source="graphics/tileset.png" width="512" height="512"/>
+ <tile id="1">
+  <properties>
+   <property name="left_height" value="0"/>
+   <property name="right_height" value="16"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="left_height" value="16"/>
+   <property name="right_height" value="32"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="left_height" value="32"/>
+   <property name="right_height" value="16"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="left_height" value="16"/>
+   <property name="right_height" value="0"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="type" value="spike"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="type" value="spike"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="left_height" value="0"/>
+   <property name="right_height" value="32"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="left_height" value="32"/>
+   <property name="right_height" value="0"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="left_height" value="0"/>
+   <property name="right_height" value="16"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="left_height" value="16"/>
+   <property name="right_height" value="32"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="left_height" value="32"/>
+   <property name="right_height" value="16"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="left_height" value="16"/>
+   <property name="right_height" value="0"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="95">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="type" value="lava"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="type" value="lava"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="type" value="lava"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="type" value="lava"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="type" value="lava"/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="type" value="lava"/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="type" value="lava"/>
+  </properties>
+ </tile>
+ <tile id="116">
+  <properties>
+   <property name="type" value="lava"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="129">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="131">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="132">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+ <tile id="133">
+  <properties>
+   <property name="type" value="block"/>
+  </properties>
+ </tile>
+</tileset>
