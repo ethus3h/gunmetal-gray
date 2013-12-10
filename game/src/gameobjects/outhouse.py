e6cb67e844590389ace9333093959b5a22ed1cc0
NathanMoore <NathanMoore@4d56c4e7-3306-40bb-bc25-9f1c267d86e0>
2013-12-10 15:47:36 +0000

    Outhouse object.
    git-svn-id: file:///var/vcs/repos/svn/trunk@82 4d56c4e7-3306-40bb-bc25-9f1c267d86e0

--- game/src/gameobjects/outhouse.py
+++ game/src/gameobjects/outhouse.py
@@ -8,16 +8,16 @@ import assets
 class Outhouse(GameObject):
     def __init__(self, scene, name, x, y, **kwargs):
         super(Outhouse, self).__init__(scene, name, x, y, **kwargs)
-        self.sprite = AnimSprite(self, assets.getSpriteAnim("anims/outhouse.json"), "closed", layer=0)
+        self.sprite = AnimSprite(self, assets.getSpriteAnim("anims/outhouse.json"), "closed", offset_y=2, layer=0)
         self.collider = SpriteCollide(self, 67, 56, 25, 103)
         self.state = 0
 
     def init(self):
-        self.scene.normal_update.append(self)
+        self.obj_mgr.normal_update.append(self)
         self.collider.addToGroup(self.obj_mgr.interactive)
 
     def destroy(self):
-        self.scene.normal_update.remove(self)
+        self.obj_mgr.normal_update.remove(self)
         self.collider.removeFromGroup(self.obj_mgr.interactive)
 
     def spriteCollide(self, gameobject, collider):
@@ -27,8 +27,17 @@ class Outhouse(GameObject):
         self.state = 1
         self.sprite.play("opening")
         obj.call("doHeal", 1000)
+        obj.call("enterOuthouse")
+        statevars.variables["spawn"] = self.name
         statevars.save()
         return True
+
+    def spawnPlayer(self):
+        """Create a player object"""
+        player = self.obj_mgr.create("Player", "player", self.x+64+16, self.y+96)
+        player.spawn("door")
+        self.sprite.play("opening")
+        self.state = 3
 
     def update(self, td):
         self.sprite.updateAnim(td)
@@ -37,6 +46,14 @@ class Outhouse(GameObject):
                 self.state = 2
                 self.sprite.play("closing")
         elif self.state == 2:
+            if not self.sprite.cursor.playing:
+                self.state = 3
+                self.sprite.play("opening")
+        elif self.state == 3:
+            if not self.sprite.cursor.playing:
+                self.state = 4
+                self.sprite.play("closing")
+        elif self.state == 4:
             if not self.sprite.cursor.playing:
                 self.state = 0
                 self.sprite.play("closed")
