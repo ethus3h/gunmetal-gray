86215b6b3ff5d727d323537c03b521bcd32c4218
ethus3h <kolubat@gmail.com>
2018-01-25 17:34:44 -0500

    Update Ember tree

--- src/components/spritecollide.py
+++ src/components/spritecollide.py
@@ -19,7 +19,7 @@ class SpriteCollide(pygame.sprite.Sprite):
         self.rect = pygame.Rect(gameobject.x + offset_x, gameobject.y + offset_y, width, height)
 
     def addToGroup(self, group):
-        print 'Added', self.gameobject, 'to', group.name
+        print 'Added', self.gameobject, 'to', group, 'with', group.__len__
         group.add(self)
 
     def removeFromGroup(self, group):
