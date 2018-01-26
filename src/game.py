a11c4d19345b819c11f1c3e9d6b64a7ec97339f9
ethus3h <kolubat@gmail.com>
2018-01-26 14:49:44 -0500

    Update Ember tree

--- src/game.py
+++ src/game.py
@@ -17,8 +17,8 @@ class Game:
     def __init__(self):
         self.config = assets.getData("config.json")
         self.scale = self.config["scale"]
-        self.width = metrics.SCREEN_WIDTH * self.scale
-        self.height = metrics.SCREEN_HEIGHT * self.scale
+        self.width = round(metrics.SCREEN_WIDTH * self.scale)
+        self.height = round(metrics.SCREEN_HEIGHT * self.scale)
 
         assets.setVolume(float(self.config["volume"]) / 100.0)
 
